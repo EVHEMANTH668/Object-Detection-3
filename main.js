977344476566e61243413ce50898f19e2f6cf3bd
E-V-HEMANTH <74523620+EVHEMANTH668@users.noreply.github.com>
2022-09-03 18:36:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var canvas = "";
+stats = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerText = " STATUS : Detecting Objects"
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+
+}
+  
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function draw (){
+    image(video,0,0,640,420);
+    
+    if (stats != ""){
+        r = random(255)
+        g = random(255)
+        b = random(255)
+        for(i=0; i<objects.length; i++){
+        fill(r,g,b);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x+15 , objects[i].y+15);
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        document.getElementById("numebr_of_objects").innerText = "Number of objects detected are" + objects.length;
+    }
+}
+}
+
+
+function modelLoaded(){
+    console.log('MODEL LOADED');
+    stats = "true";
+    objectDetector.detect(video,gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
